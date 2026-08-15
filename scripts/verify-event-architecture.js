50d954c411201b3e765063879573be9e3f2aee64
TasupK <66463522+TasupK@users.noreply.github.com>
2026-08-15 18:59:51 +0900

    test: require explicit event api filenames

--- scripts/verify-event-architecture.js
+++ scripts/verify-event-architecture.js
@@ -17,24 +17,30 @@ function forbidFile_(relativePath) {
   if (exists_(relativePath)) failures.push('Legacy Event architecture file still exists: ' + relativePath);
 }
 
+requireFile_('051_events/events_api.gs');
+requireFile_('052_applicants/applicants_api.gs');
 requireFile_('053_payment/payment_service.gs');
 requireFile_('053_payment/payment_sheet_dao.gs');
-requireFile_('054_attendance/attendance.gs');
+requireFile_('054_attendance/attendance_api.gs');
 requireFile_('054_attendance/attendance_sheet_dao.gs');
 requireFile_('054_attendance/event_attendance.gs');
-requireFile_('055_refunds/refunds.gs');
+requireFile_('055_refunds/refunds_api.gs');
 requireFile_('055_refunds/refunds_sheet_dao.gs');
 requireFile_('055_refunds/event_refunds.gs');
 requireFile_('056_files/event_files.gs');
 
+forbidFile_('051_events/events.gs');
+forbidFile_('052_applicants/applicants.gs');
 forbidFile_('050_common/event_payments.gs');
 forbidFile_('050_common/event_payment_sheet_dao.gs');
 forbidFile_('053_attendance/attendance.gs');
 forbidFile_('053_attendance/attendance_sheet_dao.gs');
 forbidFile_('053_attendance/event_attendance.gs');
+forbidFile_('054_attendance/attendance.gs');
 forbidFile_('054_refunds/refunds.gs');
 forbidFile_('054_refunds/refunds_sheet_dao.gs');
 forbidFile_('054_refunds/event_refunds.gs');
+forbidFile_('055_refunds/refunds.gs');
 forbidFile_('055_files/event_files.gs');
 
 if (failures.length) {
