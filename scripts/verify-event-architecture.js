9c17da7f4060d33871104ca6b18235c862accaa3
TasupK <66463522+TasupK@users.noreply.github.com>
2026-08-15 19:03:07 +0900

    test: require explicit event file service boundary

--- scripts/verify-event-architecture.js
+++ scripts/verify-event-architecture.js
@@ -30,7 +30,7 @@ requireFile_('054_attendance/attendance_service.gs');
 requireFile_('054_attendance/attendance_sheet_dao.gs');
 requireFile_('055_refunds/refunds_api.gs');
 requireFile_('055_refunds/refunds_sheet_dao.gs');
-requireFile_('056_files/event_files.gs');
+requireFile_('056_files/event_file_service.gs');
 
 forbidFile_('050_common/event_payments.gs');
 forbidFile_('050_common/event_payment_sheet_dao.gs');
@@ -49,6 +49,7 @@ forbidFile_('054_refunds/event_refunds.gs');
 forbidFile_('055_refunds/refunds.gs');
 forbidFile_('055_refunds/event_refunds.gs');
 forbidFile_('055_files/event_files.gs');
+forbidFile_('056_files/event_files.gs');
 
 if (failures.length) {
   failures.forEach(function (failure) { console.error(failure); });
