4cf92dc8ed3261f4baea3cb1704c22d7da288d24
TasupK <66463522+TasupK@users.noreply.github.com>
2026-08-15 18:58:01 +0900

    test: require renumbered event feature folders

--- scripts/verify-event-architecture.js
+++ scripts/verify-event-architecture.js
@@ -19,8 +19,23 @@ function forbidFile_(relativePath) {
 
 requireFile_('053_payment/payment_service.gs');
 requireFile_('053_payment/payment_sheet_dao.gs');
+requireFile_('054_attendance/attendance.gs');
+requireFile_('054_attendance/attendance_sheet_dao.gs');
+requireFile_('054_attendance/event_attendance.gs');
+requireFile_('055_refunds/refunds.gs');
+requireFile_('055_refunds/refunds_sheet_dao.gs');
+requireFile_('055_refunds/event_refunds.gs');
+requireFile_('056_files/event_files.gs');
+
 forbidFile_('050_common/event_payments.gs');
 forbidFile_('050_common/event_payment_sheet_dao.gs');
+forbidFile_('053_attendance/attendance.gs');
+forbidFile_('053_attendance/attendance_sheet_dao.gs');
+forbidFile_('053_attendance/event_attendance.gs');
+forbidFile_('054_refunds/refunds.gs');
+forbidFile_('054_refunds/refunds_sheet_dao.gs');
+forbidFile_('054_refunds/event_refunds.gs');
+forbidFile_('055_files/event_files.gs');
 
 if (failures.length) {
   failures.forEach(function (failure) { console.error(failure); });
